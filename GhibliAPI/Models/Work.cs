115c0b61f0243a13326b420da0a3e428565b8b7d
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-06-13 13:20:38 -0800

    init

---
+++ GhibliAPI/Models/Work.cs
@@ -0,0 +1,13 @@
+namespace GhibliAPI.Models
+{
+  public class Work
+  {
+    public int Year { get; set; }
+    public string Director { get; set; }
+    public string Music { get; set; }
+    public int RunTime { get; set; }
+    public int Rating { get; set; }
+
+    //list of producers
+  }
+}
